b028a1c341b79061123caf7e317550cd4241e1c4
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 00:52:01 -0500

    Fixed build function

--- ssg.py
+++ ssg.py
@@ -3,8 +3,8 @@ from ssg.site import Site
 
 def main(source = "content", dest = "dist"):
     config = {
-        source : "source",
-        dest : "dest"
+        "source" : source,
+        "dest" : dest
     }
     Site(**config).build()
 
--- ssg/site.py
+++ ssg/site.py
@@ -7,11 +7,11 @@ class Site:
         self.dest = Path(dest)
 
     def create_dir(self, path):
-        directory = self.dest + '/' + Path.relative_to(self.source)
-        Path(directory).mkdir(parents=True, exist_ok=True)
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
 
     def build(self):
-        Path(self.dest).mkdir(parents=True, exist_ok=True)
+        self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
-            if Path(path).is_dir():
-                Path.create_dir(path)
+            if path.is_dir():
+                self.create_dir(path)
