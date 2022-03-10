a788621be7af74b3e0f1b9ed6e9e6e8da2235621
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 01:51:10 -0500

    Fixed subclass

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -24,9 +24,8 @@ class Parser:
     def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
 
-class ResourceParser:
-    def __init__(self):
-        extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+class ResourceParser(Parser):
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
     
     def parse(self, path: Path, source: Path, dest: Path):
         self.copy(path, source, dest)
