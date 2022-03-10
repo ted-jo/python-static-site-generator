eb52aa77044594022197403525961613e42db13b
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 02:35:16 -0500

    Added new Parsers

--- ssg/site.py
+++ ssg/site.py
@@ -1,5 +1,6 @@
 from os import mkdir
 from pathlib import Path
+import sys
 
 class Site:
     def __init__(self, source, dest, parsers=None):
@@ -30,5 +31,10 @@ class Site:
         if parser is not None:
             parser.parse(path, self.source, self.dest)
         else:
-            print("Not Implemented")
+            self.error("No parser for the {} extension, file skipped!").format(path.suffix)
+
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n").format(message)
+    
 
