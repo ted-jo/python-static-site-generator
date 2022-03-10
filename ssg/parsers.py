27c00872a878edab3c12228bc447798deac4370f
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 01:41:43 -0500

    fixed dest in write function

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -17,7 +17,7 @@ class Parser:
             return file.read()
 
     def write(self, path, dest, content, ext = ".html"):
-        full_path = self.dest / path.with_suffix(ext).name
+        full_path = dest / path.with_suffix(ext).name
         with open(full_path) as file:
             file.write(content)
 
