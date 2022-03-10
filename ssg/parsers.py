144fe6036d4ef060ad1be3d21e9b5cfadf6c143d
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 02:39:13 -0500

    Fixed format function

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -38,11 +38,11 @@ class ResourceParser(Parser):
 class MarkdownParser(Parser):
     extensions = [".md", ".markdown"]
 
-    def parse(self, path: Path, source: Path, dest: Path):
+    def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
         self.write(path, dest, html)
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 class ReStructuredTextParser(Parser):
     extensions = [".rst"]
@@ -51,7 +51,7 @@ class ReStructuredTextParser(Parser):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name = "html5")
         self.write(path, dest, html["html_body"])
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 
 
