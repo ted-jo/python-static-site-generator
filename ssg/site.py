4fcc488fc60d675707d072e3cb1d25cc16efd8d0
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 01:49:34 -0500

    Site --> self

--- ssg/site.py
+++ ssg/site.py
@@ -17,7 +17,7 @@ class Site:
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
-                Site.run_parser(path)
+                self.run_parser(path)
     
     def load_parser(self, extension):
         for parser in self.parsers:
