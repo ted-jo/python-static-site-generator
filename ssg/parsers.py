3ffb278dc9537913dcbb7be75d42b333c60d8660
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 01:40:57 -0500

    added self to read function

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -12,7 +12,7 @@ class Parser:
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
 
-    def read(path):
+    def read(self, path):
         with open(path) as file:
             return file.read()
 
