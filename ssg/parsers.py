424e0a05444dbc0099a21d1533007eec0cdd906c
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 02:36:31 -0500

    Typo on Restrcutured

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -44,7 +44,7 @@ class MarkdownParser(Parser):
         self.write(path, dest, html)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, content)
 
-class RestructuredTextParser(Parser):
+class ReStructuredTextParser(Parser):
     extensions = [".rst"]
 
     def parse(self, path: Path, source: Path, dest: Path):
