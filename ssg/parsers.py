c5c4d083620bdd75946d457b34f7135c11edd18d
Ted Johansmeyer <johansmeyert@gmail.com>
2022-03-10 01:46:28 -0500

    Changed scope of ResourceParser

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -18,18 +18,18 @@ class Parser:
 
     def write(self, path, dest, content, ext = ".html"):
         full_path = dest / path.with_suffix(ext).name
-        with open(full_path) as file:
+        with open(full_path, "r") as file:
             file.write(content)
 
-    def copy(path, source, dest):
+    def copy(self, path, source, dest):
         shutil.copy2(path, dest / path.relative_to(source))
 
-    class ResourceParser:
-        def __init__(self):
-            extensions = [".jpg", ".png", ".gif", ".css", ".html"]
-        
-        def parse(path: Path, source: Path, dest: Path):
-            Parser.copy(path, source, dest)
+class ResourceParser:
+    def __init__(self):
+        extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+    
+    def parse(self, path: Path, source: Path, dest: Path):
+        self.copy(path, source, dest)
 
 
 
